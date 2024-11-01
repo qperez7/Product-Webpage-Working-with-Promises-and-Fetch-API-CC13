54096159281c7cfc0f5154bb64877a8fc6ce261d
Quirianyailet Perez <qperez1@usf.edu>
2024-11-01 15:03:47 -0400

    Fetch product data and display basic information.

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 // Task 1: Setup Basic HTML Structure for Product Page (Refer to Html file)
+
+// Task 2: Fetch Products from the API Using Fetch and Promises
+const productList = document.getElementById('productList');
+
+// Fetch data from the supplier's product API
+fetch('https://www.course-api.com/javascript-store-products')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the response as JSON
+    })
+
+    .then(products => { //If the fetch is successful, extracts the product details and display them on the webpage.
+        products.forEach(product => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${product.title} ${product.image} - $${product.price} `;
+            productList.appendChild(listItem);
+        });
+    
+    })
+    
+    
+
